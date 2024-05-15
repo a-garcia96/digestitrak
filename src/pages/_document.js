f6dbab453b73d17c8b2a3b4373855dacd46b1bb6
a-garcia96 <alex.g9415@gmail.com>
2024-05-14 19:30:44 -0700

    add global color variables

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-evening-sea-50 text-evening-sea-950">
         <Main />
         <NextScript />
       </body>
