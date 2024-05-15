89cea4ce9857c6c3366fbf2b56e2b19696cd2969
a-garcia96 <alex.g9415@gmail.com>
2024-05-14 19:45:44 -0700

    add charset and viewport meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body className="bg-evening-sea-50 text-evening-sea-950">
         <Main />
         <NextScript />
