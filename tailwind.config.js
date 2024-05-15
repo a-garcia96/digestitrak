9e27dc8827b3dba1915cd3cad3a764ec1bc4ee83
a-garcia96 <alex.g9415@gmail.com>
2024-05-14 19:30:28 -0700

    Add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,22 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "evening-sea": {
+          50: "#f0fdfa",
+          100: "#cbfcf1",
+          200: "#98f7e5",
+          300: "#5cecd5",
+          400: "#2bd6c1",
+          500: "#12baa8",
+          600: "#0b968a",
+          700: "#0e776f",
+          800: "#105f5a",
+          900: "#124e4a",
+          950: "#03302f",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
