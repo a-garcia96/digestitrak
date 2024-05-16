25d3ca2696df452edcfef7e90a07d769cd294891
a-garcia96 <alex.g9415@gmail.com>
2024-05-15 22:25:12 -0700

    add full height to html and body for layout.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,12 +2,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html className="h-full" lang="en">
       <Head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <body className="bg-evening-sea-50 text-evening-sea-950">
+      <body className="h-full bg-evening-sea-50 text-evening-sea-950">
         <Main />
         <NextScript />
       </body>
