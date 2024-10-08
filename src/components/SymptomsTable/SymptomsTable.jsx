174631bad7115d4cfb81e259719a27967d72b27e
a-garcia96 <alex.g9415@gmail.com>
2024-10-07 20:06:47 -0700

    add link to add new entry

--- src/components/SymptomsTable/SymptomsTable.jsx
+++ src/components/SymptomsTable/SymptomsTable.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 import {
   Table,
@@ -11,12 +12,23 @@ import {
 
 import SeverityPill from "../SeverityPill/SeverityPill";
 
+import { PlusCircleIcon } from "@heroicons/react/20/solid";
+
 const SymptomsTable = ({ symptomsData }) => {
   return (
     <>
       <div className="shadow-md p-4 rounded bg-white space-y-2">
-        <div>
+        <div className="flex justify-between items-center">
           <h1 className="text-lg font-bold uppercase">All Symptom Logs</h1>
+          <div>
+            <Link
+              href="/symptom-logs/new"
+              className="bg-green-500 px-2 py-1 rounded text-white font-medium flex items-center gap-2"
+            >
+              <span>New</span>
+              <PlusCircleIcon className="w-5 h-5" />
+            </Link>
+          </div>
         </div>
         <Table>
           <TableHead>
@@ -27,26 +39,38 @@ const SymptomsTable = ({ symptomsData }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {symptomsData.map((log) => (
-              <TableRow key={log.id}>
-                <TableCell className="font-medium">
-                  {new Date(log.created_at)
-                    .toLocaleString("en-US")
-                    .split(", ")
-                    .map((date) => (
-                      <p>{date}</p>
-                    ))}
-                </TableCell>
+            {symptomsData.length > 0 ? (
+              symptomsData.map((log) => (
+                <TableRow key={log.id}>
+                  <TableCell className="font-medium">
+                    {new Date(log.created_at)
+                      .toLocaleString("en-US")
+                      .split(", ")
+                      .map((date) => (
+                        <p>{date}</p>
+                      ))}
+                  </TableCell>
+                  <TableCell>
+                    <div className="flex gap-x-5 gap-y-2">
+                      {log.symptoms.map((symptom) => (
+                        <SeverityPill severity={"high"}>{symptom}</SeverityPill>
+                      ))}
+                    </div>
+                  </TableCell>
+                  <TableCell>{log.notes}</TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell></TableCell>
                 <TableCell>
-                  <div className="flex gap-x-5 gap-y-2">
-                    {log.symptoms.map((symptom) => (
-                      <SeverityPill severity={"high"}>{symptom}</SeverityPill>
-                    ))}
-                  </div>
+                  <p className="text-gray-500 font-bold text-center w-full">
+                    No Entries
+                  </p>
                 </TableCell>
-                <TableCell>{log.notes}</TableCell>
+                <TableCell></TableCell>
               </TableRow>
-            ))}
+            )}
           </TableBody>
         </Table>
       </div>
