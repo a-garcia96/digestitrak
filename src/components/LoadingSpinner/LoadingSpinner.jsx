a4e8b7cae56a2036cb05c2a36f372531784671c0
a-garcia96 <alex.g9415@gmail.com>
2025-02-01 15:32:47 -0800

    add styles

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,6 +1,10 @@
-const LoadingSpinner = () => {
+const LoadingSpinner = ({ style = "color" }) => {
   return (
-    <div className="w-4 h-4 border-4 border-t-transparent border-white rounded-full animate-spin"></div>
+    <div
+      className={`w-4 h-4 border-4 border-t-transparent ${
+        style == "color" ? "border-evening-sea-500" : "border-white"
+      } rounded-full animate-spin`}
+    ></div>
   );
 };
 
