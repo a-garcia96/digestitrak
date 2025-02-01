0e4b009d0bb07b2232840120c3ce2a23f530ae6b
a-garcia96 <alex.g9415@gmail.com>
2025-02-01 15:38:52 -0800

    add dynamic rendering

--- src/components/Dashboard/ActionBanner/ActionBanner.jsx
+++ src/components/Dashboard/ActionBanner/ActionBanner.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import Link from "next/link";
 
-const ActionBanner = () => {
+const ActionBanner = ({ data }) => {
   return (
     <section className="col-span-full bg-white shadow-sm p-3 min-h-28 rounded xl:grid xl:grid-cols-12 xl:items-center">
       <div className="xl:col-span-9">
-        <h1 className="text-5xl font-bold text-evening-sea-500">
-          Hello, Alex!
+        <h1 className="text-5xl font-semibold text-evening-sea-500">
+          Hello, {data.email}
         </h1>
       </div>
       <div className="xl:col-span-3 flex justify-end gap-2">
